5f680eeeec58af37c29ec9b6009586b41871462f
Spidmax <spidmax@gmail.com>
2017-09-30 18:23:41 -0700

    Create random-circles.js

---
+++ random-circles.js
@@ -0,0 +1,17 @@
+function setup() { 
+  	createCanvas(400, 400);
+		background(255);
+		noStroke();
+} 
+
+var h = 0;
+
+function draw() { 
+  	fill(h, 100, 100);
+  	ellipse(width/2, height/2, 100, 100);
+  	h = h + 5;
+  
+  	if (h >= 360) {
+    	h = 0;
+  	}
+}
